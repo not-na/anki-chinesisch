383148c71fcfd49ac2c40fecf9c1e2097ed721fb
Anton <puppe@campus.tu-berlin.de>
2025-06-05 21:44:10 +0200

    Add Unit 9

--- tests/test_pinyin.py
+++ tests/test_pinyin.py
@@ -31,9 +31,9 @@ def test_pinyin_duplicate(note: AnkiNote, notes: List[AnkiNote]):
 
         assert len(matches) == 1, (
             f"Duplicate note with same pinyin: {extract_pinyin(note)} (Hanzi: {note.fields['Hanzi']})\n"
-            f"Duplicating hanzi: {' / '.join(n.fields['Hanzi'] for n in matches)}\n"
+            f"Duplicating DE: {' / '.join(n.fields['Deutsch'] for n in matches)}\n"
             f"Duplicating pinyin: {', '.join(extract_pinyin(n) for n in matches)} => \"{extract_pinyin(note)} [{len(matches)}]\"\n"
-            f"Duplicating DE: {' / '.join(n.fields['Deutsch'] for n in matches)}"
+            f"Duplicating hanzi: {' / '.join(n.fields['Hanzi'] for n in matches)}"
         )
 
         # Check that all notes with pinyin and with the disambiguation tag removed still do not match us
